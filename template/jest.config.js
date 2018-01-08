fd1640fff063d04b578e56b98213dc125376cfea
Julon Lou <contact@julonlou.com>
2018-01-08 11:02:24 +0100

    fix: change regex of jest transform
    It fixes the jest coverage.

--- template/jest.config.js
+++ template/jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
     "^@/(.*)$": "<rootDir>/src/$1"
   },
   transform: {
-    "^.+\\.js$": "<rootDir>/node_modules/babel-jest",
+    ".*\\.js$": "<rootDir>/node_modules/babel-jest",
     ".*\\.vue$": "<rootDir>/node_modules/vue-jest"
   },
   snapshotSerializers: ["<rootDir>/node_modules/jest-serializer-vue"],
